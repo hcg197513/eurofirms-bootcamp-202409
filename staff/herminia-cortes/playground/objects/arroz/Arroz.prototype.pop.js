d407216605f1496c8425e49c956e0f4e12197667
hcg197513 <herminiacortesgomez1975@gmail.com>
2024-10-07 18:24:16 +0200

    add modify to playground #61

---
+++ staff/herminia-cortes/playground/objects/arroz/Arroz.prototype.pop.js
@@ -0,0 +1,22 @@
+var Arroz=function() {                          
+  
+this.length= 0                                 
+}                                               
+
+Arroz.prototype.pop=function() {                 
+    var last=this[this.length -1]               
+    delete this[this.length -1]                  
+    this.length--                         
+    return last                                
+}                                                
+var animales= new Arroz                          
+animales[0]="perro"                              
+animales[1]="gato"                              
+animales[2]="caballo"                         
+animales[3]="cabra"                              
+
+animales.length=4                               
+
+var deleteElement = animales.pop()              
+console.log(deleteElement)                      
+console.log(animales)                         
